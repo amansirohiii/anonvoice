1a9b90ec6171617a81a2e1ab53fa77f5ff3950e4
Aman Sirohi <amansirohi077@gmail.com>
2024-09-25 16:10:53 +0530

    added sign-up page

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -46,7 +46,7 @@ export async function GET(request: Request){
     return Response.json(
       {
         success: true,
-        message: "Username is unique",
+        message: 'Username is unique',
 
       },
       {status: 200}
